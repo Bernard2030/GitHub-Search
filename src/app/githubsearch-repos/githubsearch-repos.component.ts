8a36a3b1b87caefe0fa861a6a3fca732a0b89c9c
Bernard2030 <bernard.mukhenye@student.moringaschool.com>
2021-08-29 14:55:37 +0300

    add content to repo html

--- src/app/githubsearch-repos/githubsearch-repos.component.ts
+++ src/app/githubsearch-repos/githubsearch-repos.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubsearchReposComponent implements OnInit {
 
-  constructor() { }
+  @Input() githubsearchRepos:any[];
+ constructor() { }
 
   ngOnInit(): void {
   }
